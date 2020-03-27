cf3db1759874baf4b57f4627ecabde5c5e2572bb
André Yuri <andre_yuri@live.com>
2020-03-27 13:27:52 -0300

    Rotas privadas e proptypes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import Route from './Routes';
+
+// Routes unsigned
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
+// Routes signed
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 
@@ -13,8 +17,8 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }
